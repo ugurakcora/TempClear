2e810acfbb0db3898eb779565f083cdbd0f486a4
Uğur <uakcora97@gmail.com>
2020-08-12 10:40:40 +0300

    first commit

---
+++ temizle.bat
@@ -0,0 +1,14 @@
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "c:\windows\temp"
+del /f /s /q "c:\windows\prefetch"
+del /s /f /q %temp%\*.*
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
